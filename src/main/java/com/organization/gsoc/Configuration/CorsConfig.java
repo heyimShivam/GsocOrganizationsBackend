7ee30e1d1dff8e7cc96ef9ad1db9377e41fd6b1d
Shivam Chaudhary <shivamthalwal@gmail.com>
2026-09-14 02:25:04 +0530

    develop

--- src/main/java/com/organization/gsoc/Configuration/CorsConfig.java
+++ src/main/java/com/organization/gsoc/Configuration/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:*")
+                        .allowedOriginPatterns("http://localhost:*", "https://gsocorganizationsfrontend.netlify.app/")
                         .allowedMethods(
                                 "GET",
                                 "POST",
